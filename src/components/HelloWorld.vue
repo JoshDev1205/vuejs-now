44778d0abe16dd62e92a355a71b90ee02e697172
Joseph Ospina Tafur <josh.999126@gmail.com>
2020-01-30 19:36:59 -0500

    agregado function serverless

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<h2>Mi nombre es: {{ name }}</h2>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+	name: 'HelloWorld',
+	props: {
+		msg: String
+	},
+	data () {
+		return {
+			name: ''
+		}
+	},
+	async created () {
+		const res = await fetch('/api/name')
+		const { name } = await res.json()
+		this.name = name
+	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
